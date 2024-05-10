cc8726a246734f46b64b4d31d0cd358060e36e5f
Darm02 <david02rangel@gmail.com>
2024-05-10 16:39:48 -0400

    float formateado

--- js/index.js
+++ js/index.js
@@ -7,44 +7,32 @@ function sumarPrecios() {
 
     let precios = document.querySelectorAll('.precio').forEach(function (precio) {
         if (precio.value != "") {
-            totalPrecios += parseFloat(precio.value.replace(/\./g, '').replace(',', '.'));
+            totalPrecios += parseFloat(precio.value);
         }
     });
 
+    let valorFormateado = totalPrecios.toLocaleString('es-ES', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
     let suma = document.getElementById('suma');
-    suma.textContent = totalPrecios;
+
+    suma.textContent = valorFormateado;
     return totalPrecios;
 }
 
 let formatearTexto = function () {
-    let inputPrecio = this;
-    let valorSinFormato = inputPrecio.value.replace(/\./g, ''); // Remover puntos existentes
-    let valorFormateado = valorSinFormato.replace(/\B(?=(\d{3})+(?!\d))/g, '.'); // Agregar puntos
-    inputPrecio.value = valorFormateado;
     sumarPrecios();
 }
-
-let manejarEntrada = function (event) {
-    if (event.keyCode === 190 || event.keyCode === 110) { // Código de la tecla de punto "."
-        event.preventDefault(); // Evitar que el punto se ingrese normalmente
-        var currentPosition = this.selectionStart; // Obtener la posición actual del cursor
-        var value = this.value;
-        this.value = value.substring(0, currentPosition) + ',' + value.substring(currentPosition + 1); // Reemplazar el punto por coma
-        this.setSelectionRange(currentPosition + 1, currentPosition + 1); // Mover el cursor una posición hacia adelante         
-    }   
-}
-
-document.querySelector('.precio').addEventListener("keydown", manejarEntrada);
 
 document.querySelector('.precio').addEventListener('input', formatearTexto);
 
 document.getElementById('agregar-precio').addEventListener('click', function (event) {
     let contenedorPrecios = document.querySelector('.precios');
     let nuevoPrecio = document.createElement('input');
-    nuevoPrecio.type = 'text';
+    nuevoPrecio.type = 'number';
     nuevoPrecio.className = 'precio';
     nuevoPrecio.addEventListener('input', formatearTexto);
-    nuevoPrecio.addEventListener('keydown', manejarEntrada);
     contenedorPrecios.appendChild(nuevoPrecio);
 });
 
